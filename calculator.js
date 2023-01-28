011aafb0044ff97928e8f87456325a3b2dc06758
kup1dman <n.sinckevich1@gmail.com>
2023-01-28 14:55:28 +0300

    Update calculator logic

--- calculator.js
+++ calculator.js
@@ -23,8 +23,8 @@ function perHour(value) {
     return (value / 24)
 }
 
-function costsPerTime(value, multiplier = 1) {
-    return value * multiplier / 1000;
+function costsPerTime(power_consumption, electricity_costs, multiplier = 1) {
+    return power_consumption * multiplier / 1000 * electricity_costs;
 }
 
 function perWeek(value) {
@@ -32,7 +32,7 @@ function perWeek(value) {
 }
 
 function addValue(value, element_id, currency_value = null){
-    const costs = ['last_1h_costs', 'last_24h_costs', 'last_7d_costs']
+    const costs = ['1h_costs', '24h_costs', '7d_costs']
     if (costs.includes(element_id)) {
         document.getElementById(element_id).textContent = `- ${parseFloat(value).toFixed(4)}` + ` ${currency_value}`
     }else {
@@ -55,6 +55,7 @@ calculator_form.addEventListener("submit", function (event) {
     const hashrate_value = calculator_form.hashrate.value;
     const power_consumption_value = calculator_form.power_consumption.value;
     const currency_value = calculator_form.currency.value;
+    const electricity_costs_value = calculator_form.electricity_costs.value;
 
     fetchPoolProfit().then(({profit}) => {
         fetchRate().then(
@@ -65,35 +66,35 @@ calculator_form.addEventListener("submit", function (event) {
                 addRow(
                  perHour(reward),
                  perHour(income),
-                 costsPerTime(power_consumption_value),
-                 perHour(income) - costsPerTime(power_consumption_value),
+                 costsPerTime(power_consumption_value, electricity_costs_value),
+                 perHour(income) - costsPerTime(power_consumption_value, electricity_costs_value),
                  currency_value,
-                 'last_1h_reward',
-                 'last_1h_income',
-                 'last_1h_costs', 
-                 'last_1h_profit')
+                 '1h_reward',
+                 '1h_income',
+                 '1h_costs', 
+                 '1h_profit')
 
                 addRow(
                  reward,
                  income, 
-                 costsPerTime(power_consumption_value, 24),
-                 income - costsPerTime(power_consumption_value, 24),
+                 costsPerTime(power_consumption_value,electricity_costs_value, 24),
+                 income - costsPerTime(power_consumption_value, electricity_costs_value, 24),
                  currency_value, 
-                 'last_24h_reward', 
-                 'last_24h_income', 
-                 'last_24h_costs', 
-                 'last_24h_profit')
+                 '24h_reward', 
+                 '24h_income', 
+                 '24h_costs', 
+                 '24h_profit')
 
                 addRow(
                  perWeek(reward), 
                  perWeek(income), 
-                 costsPerTime(power_consumption_value, 168),
-                 perWeek(income) - costsPerTime(power_consumption_value, 168),
+                 costsPerTime(power_consumption_value, electricity_costs_value, 168),
+                 perWeek(income) - costsPerTime(power_consumption_value, electricity_costs_value, 168),
                  currency_value, 
-                 'last_7d_reward', 
-                 'last_7d_income', 
-                 'last_7d_costs', 
-                 'last_7d_profit')
+                 '7d_reward', 
+                 '7d_income', 
+                 '7d_costs', 
+                 '7d_profit')
             })
     })
 });
