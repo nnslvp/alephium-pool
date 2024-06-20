ca1da20439cc46ab8bdb558aa561fdeb1c2c2409
andrei <andrei@andreis-MacBook-Air.local>
2024-06-20 18:36:06 +0300

    feat: error  uppercase

--- stats.js
+++ stats.js
@@ -375,19 +375,19 @@ function validationInput(value) {
 
   if (!value.trim().length) {
     INPUT_MIN_PAYOUTS.classList.add('invalid');
-    ERROR_MESSAGE_ELEMENT.textContent = "value can't be blank";
+    ERROR_MESSAGE_ELEMENT.textContent = "Value can't be blank";
     return false;
   } 
 
   if (isNegativeNumeric(value)) {
     INPUT_MIN_PAYOUTS.classList.add('invalid');
-    ERROR_MESSAGE_ELEMENT.textContent = 'value should be positive number';
+    ERROR_MESSAGE_ELEMENT.textContent = 'Value should be positive number';
     return false
   } 
   
   if (!isNumeric(value)) {
     INPUT_MIN_PAYOUTS.classList.add('invalid');
-    ERROR_MESSAGE_ELEMENT.textContent = 'value should be number';
+    ERROR_MESSAGE_ELEMENT.textContent = 'Value should be number';
     return false;
   } 
   
@@ -441,7 +441,7 @@ function handleError(error) {
     ''
   );
   INPUT_MIN_PAYOUTS.classList.add('invalid');
-  ERROR_MESSAGE_ELEMENT.textContent = `Error: ${sanitizedMessage}`;
+  ERROR_MESSAGE_ELEMENT.textContent = `${sanitizedMessage}`;
 }
 
 init();
